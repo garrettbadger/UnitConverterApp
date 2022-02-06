5cd161d841f5d3c89a646fb1b3e6d4e441f02489
garrettbadger <garrett.badger.id@gmail.com>
2022-02-05 21:16:31 -0700

    polished comments

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -11,27 +11,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        Binding allows the programmer to more easily access items in the xml.
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calculateCups.setOnClickListener { calculateCups() }
     }
 
     fun calculateCups(){
-        val stringInTextField = binding.cups.text.toString()
-        val cups = stringInTextField.toDoubleOrNull()
+//        This function converts the cups input by the user to the units selected by the user.
+        val stringInTextField = binding.cups.text.toString()//Stores user input for cups.
+        val cups = stringInTextField.toDoubleOrNull()//Converts the string to a double.
         if (cups == null){
             binding.cupsResult.text="Converted Unit:"
             return
         }
-        val selectedId = binding.cupsOptions.checkedRadioButtonId
-        val conversion = when(selectedId){
+        val selectedId = binding.cupsOptions.checkedRadioButtonId//stores id of which radio button is selected.
+        val conversion = when(selectedId){ //determines which unit is selected and is assigned appropriate value.
             R.id.tablespoons -> 16.0 //multiply cups by 16
             R.id.milliliters -> 236.58//multiply cups by 236.58
             else -> 8.0//multiply cups by 8
         }
-        var cupsResult = cups * conversion
+        var cupsResult = cups * conversion //converts cups to the unit.
 
-        binding.cupsResult.text = cupsResult.toString()
+        binding.cupsResult.text = cupsResult.toString() //outputs conversion to the user.
 
     }
 }
